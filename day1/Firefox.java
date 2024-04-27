1e0f93c5f501ba5f989026e8831309ed2edc8588
Charumithaa <74087953+charumithaa@users.noreply.github.com>
2024-04-27 23:51:26 +0530

    Add files via upload

---
+++ day1/Firefox.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		int releaseYear = 1998;
+		String browserName = "firefox";
+		float browserVersion = 100.2f;
+		char browserLogo = 'f';
+		boolean isVisible = true;
+		System.out.println(releaseYear+" "+browserName+" "+browserVersion+" "+browserLogo+" "+isVisible);
+		
+
+	}
+
+}
---
+++ day1/HelloTestLeaf.java
@@ -0,0 +1,9 @@
+package week1.day1;
+
+public abstract class HelloTestLeaf {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Testleaf");
+	}
+
+}
